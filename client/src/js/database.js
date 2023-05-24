7aaf81b62d242130879ccade5b8e2cb688211768
Michelle <michellebaker1129@gmail.com>
2023-05-24 11:51:44 -0700

    worked thru class

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,46 @@
-// TODO: Install the following package:
+//  Install the following package:
 import { openDB } from 'idb';
 
-// TODO: Complete the initDb() function below:
+//  Complete the initDb() function below:
 const initdb = async () => {
-
+openDB("contactDB", 1, {
+    upgrade(db) {
+        if (db.objectStoreNames.contains('contactDB')) {
+            return;
+        }
+        db.createObjectStore('contactDB', { keyPath: 'id', autoIncrement: true });
+    }
+})
 };
 
 
-// TODO: Complete the postDb() function below:
+// Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+ const contactDB = await openDB('contactDB', 1);
+ const tx = contactDB.transaction('contactDB', 'readwrite');
+ const store = tx.objectStore('contactDB');
+ const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email, });
 };
 
-// TODO: Complete the getDb() function below:
+// Complete the getDb() function below:
 export const getDb = async () => {
-  
+  const contactDB = await openDB('contactDB', 1);
+  const tx = contactDB.transaction('contactDB', 'readonly');
+  const store = tx.objectStore('contactDB');
+  const request = store.getAll();
+  const result = await request;
+  return result;
 };
 
-// TODO: Complete the deleteDb() function below:
+// Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  const contactDB = await openDB('conactDB', 1);
+  const tx = contactDB.transaction('contactDB', 'readwrite');
+  const store = tx.objectStore('contactDB');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
